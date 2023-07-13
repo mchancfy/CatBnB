f59310bfc2a245556311281d28820155a387e4ba
Philip Curry <muttaphilly@gmail.com>
2023-07-13 14:01:21 +0200

    reload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # avatar may break
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
